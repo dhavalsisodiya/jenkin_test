794c16d84295008a1585cc830ec6057dea07bf5e
Vidhyut Pandya <vidhyut@e2logy.com>
2018-03-27 17:22:03 +0530

    no message

--- sql_scripts/V1.3.3__create_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `flyway_migration_test` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `dob` datetime NOT NULL,
-  `date_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `address` varchar(500) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1
